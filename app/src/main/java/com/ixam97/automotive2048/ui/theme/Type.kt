0e087826810efb3264912eb63afdab3b3fa36ba4
Maximilian Goldschmidt <ixam97@ixam97.de>
2024-08-31 03:32:39 +0200

    added check for Polestar Font

--- app/src/main/java/com/ixam97/automotive2048/ui/theme/Type.kt
+++ app/src/main/java/com/ixam97/automotive2048/ui/theme/Type.kt
@@ -1,5 +1,6 @@
 package com.ixam97.automotive2048.ui.theme
 
+import android.util.Log
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -39,25 +40,35 @@ val Typography = Typography(
     labelLarge = TextStyle(fontSize = 31.sp)
 )
 
-val defaultPolestarTypography = Typography(
-    displayLarge = Typography.displayLarge.copy(fontFamily = polestarFont),
-    displayMedium = Typography.displayMedium.copy(fontFamily = polestarFont),
-    displaySmall = Typography.displaySmall.copy(fontFamily = polestarFont),
+val defaultPolestarTypography = try {
+    if (File("/product/fonts/PolestarUnica77-Regular.otf").exists() && File("/product/fonts/PolestarUnica77-Medium.otf").exists()) {
+        Typography(
+            displayLarge = Typography.displayLarge.copy(fontFamily = polestarFont),
+            displayMedium = Typography.displayMedium.copy(fontFamily = polestarFont),
+            displaySmall = Typography.displaySmall.copy(fontFamily = polestarFont),
 
-    headlineLarge = Typography.headlineLarge.copy(fontFamily = polestarFont),
-    headlineMedium = Typography.headlineMedium.copy(fontFamily = polestarFont),
-    headlineSmall = Typography.headlineSmall.copy(fontFamily = polestarFont),
+            headlineLarge = Typography.headlineLarge.copy(fontFamily = polestarFont),
+            headlineMedium = Typography.headlineMedium.copy(fontFamily = polestarFont),
+            headlineSmall = Typography.headlineSmall.copy(fontFamily = polestarFont),
 
-    titleLarge = Typography.titleLarge.copy(fontFamily = polestarFont,),
-    titleMedium = Typography.titleMedium.copy(fontFamily = polestarFont),
-    titleSmall = Typography.titleSmall.copy(fontFamily = polestarFont),
+            titleLarge = Typography.titleLarge.copy(fontFamily = polestarFont,),
+            titleMedium = Typography.titleMedium.copy(fontFamily = polestarFont),
+            titleSmall = Typography.titleSmall.copy(fontFamily = polestarFont),
 
-    bodyLarge = Typography.bodyLarge.copy(fontFamily = polestarFont),
-    bodyMedium = Typography.bodyMedium.copy(fontFamily = polestarFont),
-    bodySmall = Typography.bodySmall.copy(fontFamily = polestarFont),
+            bodyLarge = Typography.bodyLarge.copy(fontFamily = polestarFont),
+            bodyMedium = Typography.bodyMedium.copy(fontFamily = polestarFont),
+            bodySmall = Typography.bodySmall.copy(fontFamily = polestarFont),
 
-    labelLarge = Typography.labelLarge.copy(fontFamily = polestarFont),
-    labelMedium = Typography.labelMedium.copy(fontFamily = polestarFont),
-    labelSmall = Typography.labelSmall.copy(fontFamily = polestarFont)
-)
+            labelLarge = Typography.labelLarge.copy(fontFamily = polestarFont),
+            labelMedium = Typography.labelMedium.copy(fontFamily = polestarFont),
+            labelSmall = Typography.labelSmall.copy(fontFamily = polestarFont)
+        )
+    } else {
+        Log.w("TYPE", "Polestar Font not found")
+        Typography
+    }
+} catch (e: Exception) {
+    Log.e("TYPE", "Polestar Font not found")
+    Typography
+}
 
